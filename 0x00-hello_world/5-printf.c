d5cbde0281957bc47c270128e6566d1cf0fc4987
BlackBond06 <eleazuosita007@yahoo.com>
2023-03-16 10:16:28 -0700

    shell command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.sh>
+
+/**
+ * main - a function that prints a given message
+ *
+ * Return: returns 0 if successful.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
