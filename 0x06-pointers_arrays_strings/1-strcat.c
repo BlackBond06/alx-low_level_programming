d4b092af17587fa71651a0b45110ca181c59623b
BlackBond06 <eleazuosita007@yahoo.com>
2023-03-29 03:34:55 -0700

    C pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/1-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ * Return: void
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+int j;
+
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This folder contains codes for working with C pointers, arrays and string.
