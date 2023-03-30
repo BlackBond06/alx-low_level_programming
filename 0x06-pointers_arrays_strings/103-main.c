995aabacc79de5513366ef055aed58827e1acb40
BlackBond06 <eleazuosita007@yahoo.com>
2023-03-30 06:30:33 -0700

    C pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/103-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+return (0);
+
+}
