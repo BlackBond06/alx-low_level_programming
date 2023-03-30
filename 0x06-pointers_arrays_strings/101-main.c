ccc60b7038816b737f4a80e3d1e09c0e77588b95
BlackBond06 <eleazuosita007@yahoo.com>
2023-03-30 03:12:01 -0700

    C pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * string_toupper - converts strings from lower case to upper case
+ * Return: Always 0
+ */
+int main(void)
+{
+
+void print_number(int n);
+return (0);
+
+}
