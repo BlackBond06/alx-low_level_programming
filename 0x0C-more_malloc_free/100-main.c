6946c9525f6176f7e6d345b95c50c59fc9865731
BlackBond06 <eleazuosita007@yahoo.com>
2023-04-13 04:49:38 -0700

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/100-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+return (0);
+
+}
