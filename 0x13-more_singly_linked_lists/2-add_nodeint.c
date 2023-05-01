f50aa02c90dc6c06ca472c55b70c862a6ea92e00
BlackBond06 <eleazuosita007@yahoo.com>
2023-05-01 05:10:10 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,11 +3,11 @@
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list.
  * @head: the entry point into a linked list
- * @x: data to insert in that new node
+ * @n: data to insert in that new node
  *
  * Return: pointer to the new node, or NULL if it fails
  */
-listint_t *add_nodeint(listint_t **head, const int x)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 listint_t *new;
 
@@ -15,7 +15,7 @@ new = malloc(sizeof(listint_t));
 if (!new)
 return (NULL);
 
-new->x = x;
+new->n = n;
 new->next = *head;
 *head = new;
 
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,11 +3,11 @@
 /**
  * add_nodeint_end - that adds a new node at the end of a listint_t list.
  * @head: Entry point into the linked list
- * @x: data to insert in the new element
+ * @n: data to insert in the new element
  *
  * Return: pointer to the new node, or NULL if it fails
  */
-listint_t *add_nodeint_end(listint_t **head, const int x)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 listint_t *new;
 listint_t *temp = *head;
@@ -16,7 +16,7 @@ new = malloc(sizeof(listint_t));
 if (!new)
 return (NULL);
 
-new->x = x;
+new->n = n;
 new->next = NULL;
 
 if (*head == NULL)
