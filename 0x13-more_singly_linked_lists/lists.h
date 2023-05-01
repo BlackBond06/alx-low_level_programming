c6b9bf60a61b0c86fc3eae6a62e69fac6e716306
BlackBond06 <eleazuosita007@yahoo.com>
2023-05-01 03:14:31 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -6,7 +6,7 @@
 
 /**
  * struct listint_s - singly linked list
- * @n: integer
+ * @x: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
@@ -14,14 +14,14 @@
  */
 typedef struct listint_s
 {
-int n;
+int x;
 struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+listint_t *add_nodeint(listint_t **head, const int x);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int x);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
