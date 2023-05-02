3d7e6bbebb2fdaf35f387577742c361091aa1fcb
BlackBond06 <eleazuosita007@yahoo.com>
2023-05-02 06:49:30 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index index of a listint_t linked list
+ * delete_nodeint_at_index - deletes node at  index of a listint_t linked list
  * @head: Entry point into the linked list
  * @index: index of the node to be deleted
  *
