82399a6f525f71718f06cdc0e21f7839245ea23e
BlackBond06 <eleazuosita007@yahoo.com>
2023-07-07 05:12:30 -0700

    prints binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_binary -  prints the binary representation of a num
+ *
+ * Parameter
+ * @n: int converted to its binary value
+ *
+ */
+
+void print_binary(unsigned long int n)
+{
+	int counter = 0;
+	unsigned long int current_value;
+
+	for (int j = 63; j >= 0; j--)
+	{
+		current_value = n >> j;
+
+		if (current_value & 1)
+		{
+			_putchar('1');
+			counter++;
+		}
+		else if (counter)
+			_putchar('0');
+	}
+	if (!counter)
+		_putchar('0');
+}
+
+
+
+
+
+}
